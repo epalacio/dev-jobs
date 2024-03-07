ee4fedf5b771d2518b6c89b2a274e7cee48681f9
Esteban Palacio <esteban.palaciomartinez@publicissapient.com>
2024-03-07 11:57:08 -0500

    add main button, and custom fontsize

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,20 +7,22 @@ const config: Config = {
 		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
 	],
 	theme: {
-		colors: {
-			violet: '#5964e0',
-			lightViolet: '#939BF4',
-			veryDarkBlue: '#19202D',
-			midnight: '#121721',
-			darkGrey: '#6E8098',
-			gray: '#9DAEC2',
-			lightGray: '#F4F6F8',
+		fontSize: {
+			sm: ['14px', '18px'],
+			base: ['16px', '26px'],
+			lg: ['20px', '24px'],
+			xl: ['24px', '29px'],
+			'2xl': ['28px', '34px'],
 		},
 		extend: {
-			backgroundImage: {
-				'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-				'gradient-conic':
-					'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+			colors: {
+				violet: '#5964e0',
+				lightviolet: '#939BF4',
+				verydarkblue: '#19202D',
+				midnight: '#121721',
+				darkgray: '#6E8098',
+				gray: '#9DAEC2',
+				lightgray: '#F4F6F8',
 			},
 			fontFamily: {
 				KumbhSans: ['KumbhSans', 'sans-serif'],
