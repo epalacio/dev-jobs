59346ed41f283f839debb4ec478851a7f8067db7
Esteban Palacio <esteban.palaciomartinez@publicissapient.com>
2024-03-11 11:23:34 -0400

    create footer for specific page;

---
+++ src/components/Footer.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Image from 'next/image';
+
+import Button from './Button.client';
+
+interface FooterProps {
+	companyName: string;
+	jobTitle: string;
+}
+
+const Footer: React.FC<FooterProps> = ({ companyName, jobTitle }) => {
+	return (
+		<div className='flex flex-col p-6 bg-white dark:bg-verydarkblue text-base w-11/12 my-12 rounded-t-lg md:flex-row md:items-center md:justify-between'>
+			<div className='flex flex-col items-center md:flex-row hidden md:block'>
+				<div className='md:flex md:flex-col justify-center text-center md:text-left'>
+					<h3 className='text-xl text-verydarkblue dark:text-white hover:text-gray py-1'>
+						{jobTitle}
+					</h3>
+					<p className='text-lg dark:text-darkgray mt-2'>{companyName}</p>
+				</div>
+			</div>
+			<Button ctaText={'Apply Now'} />
+		</div>
+	);
+};
+
+export default Footer;
